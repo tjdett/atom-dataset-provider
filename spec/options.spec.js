fbe2ffcd5d16cac94f2c7fba2fba244fcbd339f9
Tim Dettrick <t.dettrick@uq.edu.au>
2012-02-03 15:49:26 +1000

    Added binary and default directory.

--- spec/options.spec.js
+++ spec/options.spec.js
@@ -2,11 +2,16 @@
 var vows = require('vows');
 var should = require('should');
 
-var providerOptions = require('../lib/atom-dataset-provider/options');
+var optionsParser = require('../lib/atom-dataset-provider/options');
 
 vows.describe('Atom Provider').addBatch({
     'the CLI parser': {
-      topic: providerOptions,
+      topic: optionsParser,
+      "defaults directory to '.'": function(parser) {
+        parser.parse("node myapp.js".split(" "));
+        should.exist(parser.directory);
+        parser.directory.should.equal('.');
+      },
       "takes directory to monitor": function(parser) {
         parser.parse("node myapp.js --directory myDir".split(" "));
         should.exist(parser.directory);
