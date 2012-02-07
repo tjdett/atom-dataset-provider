a8db87cb9d790414fc4519d22eb1c98586001bb1
Tim Dettrick <t.dettrick@uq.edu.au>
2012-02-07 11:08:04 +1000

    Rudimentary feed now available.

--- spec/options.spec.js
+++ spec/options.spec.js
@@ -7,13 +7,15 @@ vows.describe('CLI Parser').addBatch({
       topic: require('../lib/atom-dataset-provider/options'),
       "defaults directory to '.'": function(parser) {
         parser.parse("node myapp.js".split(" "));
-        should.exist(parser.directory);
-        parser.directory.should.equal('.');
+        parser.should.have.property('directory', '.');
       },
       "takes directory to monitor": function(parser) {
         parser.parse("node myapp.js --directory myDir".split(" "));
-        should.exist(parser.directory);
-        parser.directory.should.equal('myDir');
+        parser.should.have.property('directory', 'myDir');
+      },
+      "takes port to serve on": function(parser) {
+        parser.parse("node myapp.js --port 4000".split(" "));
+        parser.should.have.property('port', 4000);
       },
     }
 }).export(module);
