1a80c6e6f7e152b4fa939f0fbe098a429e543959
Tim Dettrick <t.dettrick@uq.edu.au>
2012-02-07 10:30:24 +1000

    Templater works for simple cases.

--- spec/templater.spec.js
+++ spec/templater.spec.js
@@ -21,11 +21,56 @@ vows.describe('Templater').addBatch({
           parser = new FeedMe();
           parser.write(result);
           feed = parser.done();
-          console.dir(feed);
           feed.type.should.equal('atom');
           feed.id.should.equal('test');
           feed.title.should.equal('test');
+          feed.updated.should.match(/^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d.\d{3}Z$/);
           should.not.exist(feed.items);
+        }
+      },
+      'when run with a single dataset': {
+        topic: function(templater) {
+          templater.render({
+              'id': 'test',
+              'title': 'test',
+              'datasets': [{
+                  'author': {'name': 'fred'},
+                  'files': [{
+                    'href': 'general/a.txt',
+                    'title': 'a.txt',
+                    'type': 'text/plain'
+                  }],
+                  'updated': new Date(1970, 00, 03)
+              }]
+          }, this.callback);
+        },
+        "should produce a valid single-entry Atom feed": function(result) {
+          parser = new FeedMe();
+          parser.on('error', function(err) {
+              console.dir(result);
+              should.fail(err);
+          });
+          parser.write(result);
+          feed = parser.done();
+          // Check feed details
+          feed.type.should.equal('atom');
+          feed.id.should.equal('test');
+          feed.title.should.equal('test');
+          // Feed "updated" should match dataset
+          feed.updated.should.match(/^1970-01-\d\dT\d\d:\d\d:\d\d.\d{3}Z$/);
+          // There should be one item
+          should.exist(feed.items);
+          feed.items.should.be.length(1);
+          var item = _.first(feed.items);
+          item.id.should.match(/^general\/a.txt@1970-01/);
+          item.author.name.should.equal('fred');
+          item.title.should.match(/^fred - 1970-01-/);
+          item.updated.should.equal(feed.updated);
+          with ({enclosure: item.link}) {
+            enclosure.should.have.property('href');
+            enclosure.should.have.property('title');
+            enclosure.should.have.property('type');
+          }
         }
       }
     }
