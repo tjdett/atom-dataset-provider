469013462f4e85f043aebd156d4712db60f4c886
Tim Dettrick <t.dettrick@uq.edu.au>
2012-02-09 15:55:10 +1000

    Implemented paging support in directory scanner.

--- spec/directory_scanner.spec.js
+++ spec/directory_scanner.spec.js
@@ -18,8 +18,11 @@ vows.describe('Directory Scanner').addBatch({
           scanner.scan(testDir, {}, this.callback);
         },
         "should provide an empty list of datasets": function(err, scanResult) {
-          scanResult.should.be.instanceof(Array);
-          scanResult.should.have.length(0);
+          scanResult.should.be.instanceof(Object);
+          scanResult.datasets.should.be.instanceof(Array);
+          scanResult.datasets.should.have.length(0);
+          scanResult.should.not.have.property('next');
+          scanResult.should.not.have.property('prev');
         }
       },
       'when run on a directory with files "a.txt", "b.txt" & "c .txt"': {
@@ -44,12 +47,13 @@ vows.describe('Directory Scanner').addBatch({
             });
         },
         "should provide a list of three datasets": function(err, scanResult) {
-          scanResult.should.be.instanceof(Array);
-          scanResult.should.have.length(3);
+          scanResult.should.be.instanceof(Object);
+          scanResult.datasets.should.be.instanceof(Array);
+          scanResult.datasets.should.have.length(3);
         },
         "should in a dataset": {
           topic: function(scanResult) {
-            return scanResult[0];
+            return _.first(scanResult.datasets);
           },
           'have a list of files with "href", "length", "title" & "type"': 
             function(err, dataset) {
@@ -67,16 +71,14 @@ vows.describe('Directory Scanner').addBatch({
           }
         },
         "should produce datasets in reverse order": function(err, scanResult) {
-          scanResult.should.be.instanceof(Array);
-          files = _.chain(scanResult)
+          scanResult.datasets.should.be.instanceof(Array);
+          files = _.chain(scanResult.datasets)
             .pluck('files')
             .flatten()
             .pluck('href')
             .value();
           // Check file array against given
-          _.each(_.zip(files, ['c .txt','b.txt','a.txt']), function(v) {
-             _.first(v).should.equal(_.last(v));
-          });
+          files.should.eql(['c .txt','b.txt','a.txt']);
         }
       },
       'when run on a directory with files "a.tif" & "a.txt"': {
@@ -100,17 +102,19 @@ vows.describe('Directory Scanner').addBatch({
             });
         },
         "there should be one dataset": function(err, scanResult) {
-          scanResult.should.be.instanceof(Array);
-          scanResult.should.have.length(1);
+          scanResult.should.be.instanceof(Object);
+          scanResult.datasets.should.be.instanceof(Array);
+          scanResult.datasets.should.have.length(1);
         },
         "the single dataset should have two files": function(err, scanResult) {
-          _.first(scanResult).files.should.be.instanceof(Array);
-          _.first(scanResult).files.should.have.length(2);
+          _.first(scanResult.datasets).files.should.be.instanceof(Array);
+          _.first(scanResult.datasets).files.should.have.length(2);
         }
       },
       'when run on a directory with 10 files with a limit of 3': {
         topic: function(scanner) {
           var testDir = temp.mkdirSync('atom-dataset-provider-test-data-');
+          scanner.directory = testDir;
           var topicCallback = this.callback;
           var testData = _.zip(
             _.map(_.range(1, 11), function(i) { return i+".txt" }),
@@ -130,11 +134,99 @@ vows.describe('Directory Scanner').addBatch({
         },
         "only the three files most recent files should be returned": 
           function(err, scanResult) {
-            scanResult.should.have.length(3);
-            _.first(scanResult[0].files).href.should.equal('10.txt');
-            _.first(scanResult[1].files).href.should.equal('9.txt');
-            _.first(scanResult[2].files).href.should.equal('8.txt');
+            scanResult.should.be.instanceof(Object);
+            scanResult.datasets.should.be.instanceof(Array);
+            scanResult.datasets.should.have.length(3);
+            files = _.map(_.pluck(scanResult.datasets, 'files'),_.first);
+            files[0].href.should.equal('10.txt');
+            files[1].href.should.equal('9.txt');
+            files[2].href.should.equal('8.txt');
+          },
+        "a next() function should be provided": function(err, scanResult) {
+          scanResult.should.have.property('next');
+          scanResult.next.should.be.a('function');
+          scanResult.next().should.have.property('before');
+          scanResult.next().should.have.property('limit');
+        },
+        "a prev() function should not be provided": function(err, scanResult) {
+          scanResult.should.not.have.property('prev');
+        },
+        "and when run using next() details": {
+          topic: function(scanResult, scanner) {
+            scanner.scan(scanner.directory, scanResult.next(), this.callback);
+          },
+          "the next three files most recent files should be returned": 
+            function(err, scanResult) {
+              scanResult.should.be.instanceof(Object);
+              scanResult.datasets.should.be.instanceof(Array);
+              scanResult.datasets.should.have.length(3);
+              files = _.map(_.pluck(scanResult.datasets, 'files'),_.first);
+              files[0].href.should.equal('7.txt');
+              files[1].href.should.equal('6.txt');
+              files[2].href.should.equal('5.txt');
+            },
+          "a next() function should be provided": function(err, scanResult) {
+            scanResult.should.have.property('next');
+            scanResult.next.should.be.a('function');
+            scanResult.next().should.have.property('before');
+            scanResult.next().should.have.property('limit');
+          },
+          "a prev() function should be provided": function(err, scanResult) {
+            scanResult.should.have.property('prev');
+            scanResult.prev.should.be.a('function');
+            scanResult.prev().should.have.property('after');
+            scanResult.prev().should.have.property('limit');
+          },
+          "then the prev() details": {
+            topic: function(scanResult, placeholder, scanner) {
+              scanner.scan(scanner.directory, scanResult.prev(), this.callback);
+            },
+            "the previous three files should be the same": 
+            function(err, scanResult) {
+              scanResult.should.be.instanceof(Object);
+              scanResult.datasets.should.be.instanceof(Array);
+              scanResult.datasets.should.have.length(3);
+              files = _.map(_.pluck(scanResult.datasets, 'files'),_.first);
+              files[0].href.should.equal('10.txt');
+              files[1].href.should.equal('9.txt');
+              files[2].href.should.equal('8.txt');
+            },
           }
+        },
+        "and when run just 'before' from next() details": {
+          topic: function(scanResult, scanner) {
+            var options = {
+              'before': scanResult.next().before
+            };
+            scanner.scan(scanner.directory, options, this.callback);
+          },
+          "the other seven files should be returned": 
+            function(err, scanResult) {
+              scanResult.should.be.instanceof(Object);
+              scanResult.datasets.should.be.instanceof(Array);
+              scanResult.datasets.should.have.length(7);
+              _.chain(scanResult.datasets)
+                .pluck('files')
+                .map(_.first)
+                .pluck('href')
+                .zip(_.map(_.range(7, 0, -1), function(v) { return v+".txt" }))
+                .each(function(v) { 
+                    v[0].should.eql(v[1]);
+                });
+              
+            },
+          "a next() function should not be provided": 
+            function(err, scanResult) {
+              scanResult.should.not.have.property('next');
+            },
+          "a prev() function should be provided": function(err, scanResult) {
+            scanResult.should.have.property('prev');
+            scanResult.prev.should.be.a('function');
+            scanResult.prev().should.have.property('after');
+            // No limit should be present for scans without one
+            scanResult.prev().should.not.have.property('limit');
+          }
+        }
       }
     }
 }).export(module);
